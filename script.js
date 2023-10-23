a9ac4a803fe4f9a2f17d4513ceea9ee306fe2c60
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-10-23 08:18:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // write your code here
+function range(start,end) {
+	let ans = [];
+	while (start <= end) {
+		ans.push(start);
+		start++
+	}
+	return ans;
+}
 
 // do not change the code below.
 let start = parseInt(prompt("Enter Start of the Range."));
 let end = parseInt(prompt("Enter End Of the Range."));
-alert(range(start, end));
+alert(range(parseInt(start), parseInt(end)));
